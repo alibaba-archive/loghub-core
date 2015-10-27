0c01f5ae129b09072113ed547fa39262523f3941
monkerek <monkerek@hotmail.com>
2015-10-26 23:36:16 -0400

    Add kafka producer service

--- service/kafka.js
+++ service/kafka.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const kafka = require('kafka-node')
+const Producer = kafka.Producer
+const producerClient = new kafka.Client()
+const client = new Producer(producerClient);
+
+client.on('ready', function () {
+  console.log('Kafka producer is ready.')
+});
+
+client.on('error', function (err) {
+  console.error(`\n${timestamp()} - ${err.toString()}`, `\n${err.stack}`)
+})
+
+exports.client = client
