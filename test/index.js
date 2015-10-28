c6b9a2c5862f1584e4362f78742c06c213c76c73
monkerek <monkerek@hotmail.com>
2015-10-27 21:13:25 -0400

    Introduce thunk-mocha

--- test/index.js
+++ test/index.js
@@ -1,52 +1,39 @@
 'use strict'
 /*global describe, it*/
 
-const config = require('config')
 const assert = require('assert')
 const supertest = require('supertest')
 const app = require('../app')
 const request = supertest(app.server)
-const kafka = require('kafka-node')
-const Consumer = kafka.Consumer
-const client = new kafka.Client()
 const user = {_id: 123}
 const logContent = 'testlog: ' + new Date().toString()
 
 describe('Test token authorization', function () {
-  it('Without autheorization', function (done) {
-    request.get(`/log/query?log=${logContent}`)
+  it('Without autheorization', function *() {
+    yield request.get(`/log/query?log=${logContent}`)
       .expect(401)
-      .end(done)
   })
 
-  it('Token without _id provided', function (done) {
+  it('Token without _id provided', function *() {
     var token = app.signToken({})
-    request.get(`/log/query?log=${logContent}`)
+    yield request.get(`/log/query?log=${logContent}`)
       .set('Authorization', 'Bearer ' + token)
       .expect(400)
-      .end(done)
   })
 
-  it('Request without log content', function (done) {
+  it('Request without log content', function *() {
     var token = app.signToken(user)
-    request.get(`/log/query`)
+    yield request.get(`/log/query`)
       .set('Authorization', 'Bearer ' + token)
       .expect(400)
-      .end(done)
   })
 
   // TODO: Add tests for session cookie authorization.
 
-  it('Send message successfully', function (done) {
+  it('Commit logs successfully', function *() {
     var token = app.signToken(user)
-    var consumer = new Consumer(client, [{topic: config.kafkaTopic}], {autoCommit: false})
-    consumer.once('message', function (message) {
-      consumer.close(function (err) {
-        done(err)
-      })
-    })
 
-    request.get(`/log/query?log=${logContent}`)
+    yield request.get(`/log/query?log=${logContent}`)
       .set('Authorization', 'Bearer ' + token)
       .expect(200)
       .expect(function (res) {
